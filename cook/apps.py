7d200b49807a3279bd1d4f0ec95c0fbb89bf6c76
Liza Shulika <liza.shulika2009@gmail.com>
2025-04-26 11:48:36 +0300

    filled in models.py file

--- cook/apps.py
+++ cook/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CookConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cook'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cook"
