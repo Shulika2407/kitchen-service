4f4bbd203642e62851aa83f5dbbb1c638604a370
Liza Shulika <liza.shulika2009@gmail.com>
2025-04-29 21:55:57 +0300

    made class-detail and pagination

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,11 +1,32 @@
 from django.urls import path
-
 from .views import (
-   index,
-)
+    index,
+    DishTypeListView,
+    DishListView,
+    CookListView,
+    IngredientListView,
+    )
+
 
 urlpatterns = [
    path("", index, name='index'),
+   path("dishtype/",
+        DishTypeListView.as_view(),
+        name="dish-type-list",
+        ),
+   path("dish/",
+        DishListView.as_view(),
+        name="dish-list",
+        ),
+   path("cook/",
+        CookListView.as_view(),
+        name="cook-list",
+        ),
+   path("ingredient/",
+        IngredientListView.as_view(),
+        name="ingredient-list",
+        ),
+
 ]
 
 app_name="kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,54 @@
+from msilib.schema import ListView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
+
+from kitchen.models import DishType, Dish, Cook, Ingredient
+
 
 # Create your views here.
+
 def index(request):
-    return None
+    num_dishTypes = DishType.objects.count()
+    num_cook = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_ingredient = Ingredient.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_dishTypes": num_dishTypes,
+        "num_cook": num_cook,
+        "num_dishes": num_dishes,
+        "num_ingredient": num_ingredient,
+        "num_visits": num_visits + 1,
+    }
+    return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    context_object_name = "dish_type_list"
+    template_name = "kitchen/dish_type_list.html"
+    paginate_by = 5
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    context_object_name = "dish_list"
+    template_name = "kitchen/dish_list.html"
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    context_object_name = "cook_list"
+    template_name = "kitchen/cook_list.html"
+    paginate_by = 5
+
+
+class IngredientListView(generic.ListView):
+    model = Ingredient
+    context_object_name = "ingredient_list"
+    template_name = "kitchen/ingredient_list.html"
